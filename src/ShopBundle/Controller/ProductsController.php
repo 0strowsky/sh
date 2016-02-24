842734eed905f02194e6ff3bd3f8db3045e0ae33
lukaszuznanski <lukaszuznanski94@gmail.com>
2016-02-24 22:22:22 +0100

    generate view for each product in db

--- src/ShopBundle/Controller/ProductsController.php
+++ src/ShopBundle/Controller/ProductsController.php
@@ -14,9 +14,8 @@ class ProductsController extends Controller
    {	$request = Request::createFromGlobals();
    		$request->getUri();
    		$em = $this->getDoctrine()->getManager();
-   		$query = $em->createQuery('SELECT DISTINCT a.category, a.name, a.quantity, a.img, a.description FROM ShopBundle:Products a');
+   		$query = $em->createQuery('SELECT DISTINCT a.id, a.category, a.name, a.quantity, a.img, a.description FROM ShopBundle:Products a');
    		$products = $query->getResult();
-   		$current_url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-   		return $this->render('ShopBundle:Products:index.html.twig', array('products' => $products, 'request' => $request));
+  		return $this->render('ShopBundle:Products:index.html.twig', array('products' => $products, 'request' => $request));
    }
 }
