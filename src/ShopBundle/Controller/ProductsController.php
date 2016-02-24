928ac4f651ff173393cce9408e5d7725678b5cfa
lukaszuznanski <lukaszuznanski94@gmail.com>
2016-02-24 20:29:51 +0100

    view

--- src/ShopBundle/Controller/ProductsController.php
+++ src/ShopBundle/Controller/ProductsController.php
@@ -6,15 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;  
 use ShopBundle\Entity\Products;
 use ShopBundle\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProductsController extends Controller
 { 
    public function indexAction()
-   {
+   {	$request = Request::createFromGlobals();
+   		$request->getUri();
    		$em = $this->getDoctrine()->getManager();
    		$query = $em->createQuery('SELECT DISTINCT a.category, a.name, a.quantity, a.img, a.description FROM ShopBundle:Products a');
    		$products = $query->getResult();
    		$current_url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-   		return $this->render('ShopBundle:Products:index.html.twig', array('products' => $products, 'current_url' => $current_url));
+   		return $this->render('ShopBundle:Products:index.html.twig', array('products' => $products, 'request' => $request));
    }
 }
