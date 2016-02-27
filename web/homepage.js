6ce4838c343e2fa76a08d2881967ce1c6a99ec37
lukaszuznanski <lukaszuznanski94@gmail.com>
2016-02-27 19:47:07 +0100

    view fix

--- web/homepage.js
+++ web/homepage.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
+	var elementy = new Array(
+	'<IMG SRC="http://portal.holyshit.pl/images/header/hero1.png" style="height: 270px;">',
+	'<IMG SRC="http://portal.holyshit.pl/images/header/hero2.png" style="height: 270px;">',
+	'<IMG SRC="http://portal.holyshit.pl/images/header/hero3.png" style="height: 270px;">',
+	'<IMG SRC="http://portal.holyshit.pl/images/header/hero4.png" style="height: 270px;">',
+	'<IMG SRC="http://portal.holyshit.pl/images/header/hero5.png" style="height: 270px;">',
+	'<IMG SRC="http://portal.holyshit.pl/images/header/hero6.png" style="height: 270px;">',
+	'<IMG SRC="http://portal.holyshit.pl/images/header/hero7.png" style="height: 270px;">',
+	'<IMG SRC="http://portal.holyshit.pl/images/header/hero8.png" style="height: 270px;">',
+	'<IMG SRC="http://portal.holyshit.pl/images/header/hero9.png" style="height: 270px;">',
+	'<IMG SRC="http://portal.holyshit.pl/images/header/hero10.png" style="height: 270px;">'
+	);
+	var liczba = 0;
 	$('#navList li:first-child').css("border-left", "none");
 	$('#menuList li:first-child').css("border-left", "none");
+	$('#hero').html(elementy[0]).fadeIn("slow");
+	setInterval(function herosy(){
+	do { liczba = Math.random(); } while (liczba >= 1);
+	liczba = Math.floor(liczba * elementy.length);
+	return $('#hero').html(elementy[liczba]);
+	}, 8000);
+	
 });
