c76ca485eac31a84bb96fd89c5c15f4246f934f6
lukaszuznanski <lukaszuznanski94@gmail.com>
2016-02-27 03:01:02 +0100

    overriding fos view

--- src/ShopBundle/Controller/ProductsController.php
+++ src/ShopBundle/Controller/ProductsController.php
@@ -10,12 +10,16 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ProductsController extends Controller
 { 
-   public function indexAction()
+
+   public function indexAction($slug)
    {	$request = Request::createFromGlobals();
    		$request->getUri();
    		$em = $this->getDoctrine()->getManager();
    		$query = $em->createQuery('SELECT DISTINCT a.id, a.category, a.name, a.quantity, a.img, a.description FROM ShopBundle:Products a');
    		$products = $query->getResult();
+   		$params = $this->get('router')->match('/products/lenovo_y580');
+   		$uri = $this->get('router')->generate('products', array(
+   			'slug' => 'lenovo_y580'));
   		return $this->render('ShopBundle:Products:index.html.twig', array('products' => $products, 'request' => $request));
    }
 }
