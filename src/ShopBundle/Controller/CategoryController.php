1aa848d47907833242ff0ba6ce7d6dcf9d569e73
lukaszuznanski <lukaszuznanski94@gmail.com>
2016-03-01 23:02:56 +0100

    view and controllers queries

--- src/ShopBundle/Controller/CategoryController.php
+++ src/ShopBundle/Controller/CategoryController.php
@@ -11,9 +11,13 @@ class CategoryController extends Controller
 { 
    public function indexAction()
    {
+   		$rm = $this->getDoctrine()->getManager();
+        $query2 = $rm->createQuery('SELECT DISTINCT a.category FROM ShopBundle:Products a');
+        $categories2 = $query2->getResult();
+
    		$em = $this->getDoctrine()->getManager();
-   		$query = $em->createQuery('SELECT DISTINCT a.category FROM ShopBundle:Products a');
+   		$query = $em->createQuery('SELECT a.id ,a.category, a.name, a.price, a.img, a.description, a.img_min, a.display_name FROM ShopBundle:Products a');
    		$categories = $query->getResult();
-   		return $this->render('ShopBundle:Category:index.html.twig', array('categories' => $categories));
+   		return $this->render('ShopBundle:Category:index.html.twig', array('categories' => $categories, 'categories2' => $categories2));
    }
 }
--- src/ShopBundle/Controller/DefaultController.php
+++ src/ShopBundle/Controller/DefaultController.php
@@ -11,10 +11,14 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+    	$rm = $this->getDoctrine()->getManager();
+        $query2 = $rm->createQuery('SELECT DISTINCT a.category FROM ShopBundle:Products a');
+        $categories2 = $query2->getResult();
+
     	$em = $this->getDoctrine()->getManager();
         $query = $em->createQuery('SELECT DISTINCT a.id ,a.category, a.name, a.price, a.img, a.description, a.img_min, a.display_name FROM ShopBundle:Products a');
         $categories = $query->getResult();
-        return $this->render('ShopBundle:Default:index.html.twig', array('categories' => $categories));
+        return $this->render('ShopBundle:Default:index.html.twig', array('categories' => $categories, 'categories2' => $categories2 ));
     }
 
 }
