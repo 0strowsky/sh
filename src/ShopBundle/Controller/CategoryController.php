78c5f7c65900c65d9e9a8f9012f57288f51a47a5
lukaszuznanski <lukaszuznanski94@gmail.com>
2016-03-12 03:38:32 +0100

    urls fix

--- src/ShopBundle/Controller/CategoryController.php
+++ src/ShopBundle/Controller/CategoryController.php
@@ -11,7 +11,7 @@ class CategoryController extends Controller
    public function indexAction($slug)
    {
    		$rm = $this->getDoctrine()->getManager();
-        $query2 = $rm->createQuery('SELECT DISTINCT a.categorypath, a.category FROM ShopBundle:Products a');
+        $query2 = $rm->createQuery('SELECT DISTINCT a.categorypath, a.category, a.mother FROM ShopBundle:Products a');
         $categories2 = $query2->getResult();
 
    		$em = $this->getDoctrine()->getManager();
