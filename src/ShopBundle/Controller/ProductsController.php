b12bee9bfdf4c81e61da17e8874799be19df7ab1
lukaszuznanski <lukaszuznanski94@gmail.com>
2016-03-13 18:42:26 +0100

    working on product

--- src/ShopBundle/Controller/ProductsController.php
+++ src/ShopBundle/Controller/ProductsController.php
@@ -14,20 +14,26 @@ class ProductsController extends Controller
 
    public function indexAction(Request $request, $slug)
    {     
-         $userinho = $this->getUser();
-         $userId = $userinho->getId();
-
+         
          $rm = $this->getDoctrine()->getManager();
          $query2 = $rm->createQuery('SELECT DISTINCT a.categorypath, a.category, a.mother FROM ShopBundle:Products a');
          $categories2 = $query2->getResult();
 
+         $em = $this->getDoctrine()->getManager();
+         $query = $em->createQuery('SELECT DISTINCT a.id, a.category, a.name, a.price, a.img, a.description, a.display_name, a.duration FROM ShopBundle:Products a WHERE a.name = :name')->setParameter('name', $slug);
+         $products = $query->getResult();
+
+
+         $userinho = $this->getUser();
+         if($userinho == NULL){
+            return $this->render('ShopBundle:Products:index.html.twig', array('slug' => $slug, 'products' => $products, 'categories2' => $categories2));
+         }else{
+         $userId = $userinho->getId();
          $km = $this->getDoctrine()->getManager();
          $query3 = $km->createQuery('SELECT a.money FROM ShopBundle:User a WHERE a.id =:id')->setParameter('id', $userId);
          $money_value = $query3->getResult();
 
-   		$em = $this->getDoctrine()->getManager();
-   		$query = $em->createQuery('SELECT DISTINCT a.id, a.category, a.name, a.price, a.img, a.description, a.display_name, a.duration FROM ShopBundle:Products a WHERE a.name = :name')->setParameter('name', $slug);
-   		$products = $query->getResult();
+   		
 
          $productform = new Products();
          $form = $this->createFormBuilder($productform)
@@ -39,5 +45,6 @@ class ProductsController extends Controller
 
 
   		return $this->render('ShopBundle:Products:index.html.twig', array('slug' => $slug, 'products' => $products, 'categories2' => $categories2, 'form' => $form->createView()));
+      }
    }
 }
